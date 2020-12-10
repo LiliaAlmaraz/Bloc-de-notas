e76ce25eefbc0db8c08bbdc18c9f8ef6369eb6fe
LiliaAlmaraz <61852167+LiliaAlmaraz@users.noreply.github.com>
2020-12-10 17:19:42 -0600

    Delete transicion.js

--- transicion.js
+++
@@ -1,9 +0,0 @@
-const colorSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-function switchColors(e){
-    if (e.target.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-    }else{
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-}
-colorSwitch.addEventListener('change', switchColors, false); 
